91467f37feed0974e9fe7af4c2a57e2c6c0bce5d
trgomez <88842062+trrgomez@users.noreply.github.com>
2022-03-19 09:36:49 -0500

    Header.jsx set up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Header() {
   return (
-    <div className='min-h-screen'>Header</div>
-  )
+    <main className="relative z-10 min-h-screen ">
+      <section className="flex flex-col justify-center items-center pt-64 px-8">
+        <h1 className="text-6xl text-sky-100 cursive leading-none lg:leading-snug home-name">
+          Hello. I'm Tirsa,
+        </h1>
+        <p className="text-3xl font-bold text-sky-300 mt-1">
+          <span className="text-3xl font-bold text-sky-300 mt-1">I'm a </span>
+          <Typewriter
+            loop
+            cursorStyle="|"
+            cursor
+            typeSpeed={100}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            words={[" Frontend Developer.", "Programmer.", "Runner.", "Star Wars Fan."]}
+          />
+        </p>
+      </section>
+    </main>
+  );
 }
+
